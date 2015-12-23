7717a4eca88a50240b5540013ad29277f75d82b3
mcxiaoke <mcxiaoke@gmail.com>
2015-12-23 21:25:42 +0800

    impl fix names

--- fix_names.py
+++ fix_names.py
@@ -3,7 +3,7 @@
 # @Author: mcxiaoke
 # @Date:   2015-12-22 23:38:39
 from __future__ import print_function, unicode_literals
-import os,sys, shutil, codecs
+import os,sys, shutil, re, codecs
 '''
 规范文件名工具
 
@@ -14,14 +14,35 @@ import os,sys, shutil, codecs
 5. 扩展名不变
 '''
 
-def process(infile):
-    path = os.path.abspath(infile)
-    fdir = os.path.dirname(path)
-    bname = os.path.basename(path)
-    name, ext = os.path.splitext(bname)
-    print(name,'+',ext)
+CHARS = '#&%.-_<>[]［］【】《》（）\'"“”'
+
+def fix_name(name):
+    for c in CHARS:
+        name = name.replace(c,' ')
+    name = name.replace('文字版','').replace('azw3','')
+    return name.replace('  ',' ').replace('  ',' ').strip()
+
+def process(infile, debug=False):
+    if debug:
+        print('debug mode, only print ,no file renamed.')
+    src = os.path.abspath(infile)
+    fdir = os.path.dirname(src)
+    sname = os.path.basename(src)
+    name, ext = os.path.splitext(sname)
+    dname = fix_name(name) + ext.lower()
+    # output = os.path.join(fdir,'output')
+    dst = os.path.join(fdir, dname)
+    if dname == sname or os.path.exists(dst):
+        print('Ignore exists:',sname)
+        return
+    print('SRC:',src)
+    print('DST:',dst)
+    if not debug:
+        shutil.move(src, dst)
+    # shutil.copy2(src, dst)
 
 if __name__ == '__main__':
     path = os.path.abspath(unicode(sys.argv[1]))
+    debug =(len(sys.argv) == 3 and sys.argv[2] == '-d')
     for n in os.listdir(path):
-        process(os.path.join(path,n))
+        process(os.path.join(path,n), debug=debug)
